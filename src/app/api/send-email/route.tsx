8a73564cd44f8e9314935d5afe374c786cae4127
kampachi0096 <kanchann033@gmail.com>
2025-03-09 21:49:09 +0900

    build

--- src/app/api/send-email/route.tsx
+++ src/app/api/send-email/route.tsx
@@ -1,6 +1,11 @@
 import { NextRequest, NextResponse } from "next/server";
 import { Resend } from "resend";
 
+const apiKey = process.env.RESEND_API_KEY;
+if (!apiKey) {
+  throw new Error('Missing API key');
+}
+
 const resend = new Resend(process.env.RESEND_API_KEY);
 
 export async function POST(req: NextRequest) {
--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div>
       {/* コンテンツ */}
-      <aside>ffffffff</aside>
+      
       <main>
         
       </main>
